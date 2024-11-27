890c69a76acc7d514a49b88addda64ecfb89f665
CocoHoshi <Carlaz020400@gmail.com>
2024-11-27 10:20:46 -0600

    Textcomponent

--- SandBoxMG/Content/Code/Scenes/GameObjects/Components/TextComponent.cs
+++ SandBoxMG/Content/Code/Scenes/GameObjects/Components/TextComponent.cs
@@ -1,14 +1,45 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SandBoxMG.Content.Code.Scenes.GameObjects.Components {
-    internal class TextComponent 
+namespace SandBoxMG.Content.Code.Scenes.GameObjects.Components 
+{
+    internal class TextComponent : Component
     {
+        #region Variables
 
 
 
+        #endregion
+        public TextComponent() 
+        {
+            
+        }
+
+        #region 
+        public override void InitializeComponent(GameObject _GORef) 
+        {
+            InitializeTextComponent();
+        }
+
+        public void InitializeTextComponent( ) 
+        {
+
+        }
+        public void LoadTextTexture(string _TextureName, ContentManager content) 
+        {
+           
+        }
+
+        public override void RenderComponent(SpriteBatch _spriteBatch) 
+        {
+            
+        }
+
+        #endregion
     }
 }
